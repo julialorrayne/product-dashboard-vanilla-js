85c8b66dcf371fbf8300d789871d15e71840a6d0
Julia <lorrayne@usf.edu>
2025-03-30 18:55:59 -0400

    Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -20,3 +20,40 @@ function fetchProductsThen() {
 //call the function to get the products
 fetchProductsThen();
 
+//Task 3:
+async function fetchProductAsync() {
+    try {
+        const response = await fetch('https://www.course-api.com/javascript-store-products');
+        if (!response.ok) {
+            throw new Error(`Error! Status: ${response.status}`); 
+        }
+
+        const products = await response.json();
+
+        displayProducts(products);
+    }catch (error) {
+        handleError(error);
+    }
+}
+function displayProducts(products) {
+    const container = document.getElementById('product-container');
+
+    products.forEach(product => {
+        const {name, price, image} = product.fields;
+
+        const productDiv = document.createElement('div');
+        productDiv.className = 'product';
+        
+        productDiv.innerHTML = `
+            <img src='${image[0].url}' alt = '${name}' />
+            <h3>${name}</h3>
+            <p>Price: $${price}</p>
+        `;
+    container.appendChild(productDiv);
+    });
+}
+function handleError(error) {
+    console.error('An error occurred:', error);
+}
+
+fetchProductAsync();
